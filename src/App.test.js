0f3c9f3a760a43ff67dee1d7975768672e14a802
burrorc <robertkarelia@gmail.com>
2021-01-18 16:26:21 -0600

    testing state

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,53 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import { shallow, mount } from 'enzyme';
+import Header from "./NEW/components/Header";
+import Footer from "./NEW/components/Footer";
+import Signup from "./NEW/components/Signup";
+import Login from "./NEW/components/Login";
+import PrivateRoute from "./NEW/components/PrivateRoute";
+import PublicRoute from "./NEW/components/PublicRoute";
+import {Link} from 'react-router-dom'
+// import Home from "./NEW/pages/Home/Home";
+// import Attendance from "./NEW/pages/Attendance/Attendance";
+// import Dashboard from "./NEW/pages/Dashboard/Dashboard";
+// import Records from "./NEW/pages/Records/Records";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const myState = {
+  isLoggedIn: false,
+      error: null,
+      userName: null,
+      userID: null,
+      userClassList: [],
+      userAttendanceRecord: [],
+      email: "",
+      password: "",
+      passwordConfirm: "",
+}
+
+describe("rendering components", () => {
+  it("renders App component without crashing", () => {
+    shallow(<App />);
+  });
+  it("renders App component without crashing", () => {
+    shallow(<Header />);
+  });
+  it("renders App component without crashing", () => {
+    shallow(<Footer />);
+  });
+  it("renders App component without crashing", () => {
+    shallow(<Signup />);
+  });
+  it("renders App component without crashing", () => {
+    shallow(<Login />);
+  });
+  it("renders App component without crashing", () => {
+    shallow(<PrivateRoute />);
+  });
+  it("renders App component without crashing", () => {
+    shallow(<PublicRoute />);
+  });
 });
+
+
+ 
