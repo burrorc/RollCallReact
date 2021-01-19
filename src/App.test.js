44f90b0da16223f40773cd155e7d76ce2fb36055
burrorc <robertkarelia@gmail.com>
2021-01-19 09:57:39 -0600

    adding state tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import App from './App';
-import { shallow, mount } from 'enzyme';
+import React from "react";
+import App from "./App";
+import { shallow, mount } from "enzyme";
 import Header from "./NEW/components/Header";
 import Footer from "./NEW/components/Footer";
 import Signup from "./NEW/components/Signup";
 import Login from "./NEW/components/Login";
 import PrivateRoute from "./NEW/components/PrivateRoute";
 import PublicRoute from "./NEW/components/PublicRoute";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 // import Home from "./NEW/pages/Home/Home";
 // import Attendance from "./NEW/pages/Attendance/Attendance";
 // import Dashboard from "./NEW/pages/Dashboard/Dashboard";
@@ -15,15 +15,15 @@ import {Link} from 'react-router-dom'
 
 const myState = {
   isLoggedIn: false,
-      error: null,
-      userName: null,
-      userID: null,
-      userClassList: [],
-      userAttendanceRecord: [],
-      email: "",
-      password: "",
-      passwordConfirm: "",
-}
+  error: null,
+  userName: null,
+  userID: null,
+  userClassList: [],
+  userAttendanceRecord: [],
+  email: "",
+  password: "",
+  passwordConfirm: "",
+};
 
 describe("rendering components", () => {
   it("renders App component without crashing", () => {
@@ -49,5 +49,19 @@ describe("rendering components", () => {
   });
 });
 
+describe("checking state", () => {
+  //const componentInstance = wrapper.instance();
+  it("begins logged out", () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    expect(wrapper.state('isLoggedIn')).toBe(false);
+  });
+  it("registers login change", () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    instance.changeLogIn();
+    expect(wrapper.state('isLoggedIn')).toBe(true);
 
- 
+  });
+  
+});
